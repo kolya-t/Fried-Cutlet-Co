3cbec56e48d0720e15368f98ed353ddb1eaccdad
kolya-t <toporkov.kolya96@gmail.com>
2016-09-29 20:23:05 +0300

    Project creation

---
+++ desktop/src/com/eninja/cutletco/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eninja.cutletco.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eninja.cutletco.CutletCo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CutletCo(), config);
+	}
+}
---
+++ html/src/com/eninja/cutletco/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.eninja.cutletco.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.eninja.cutletco.CutletCo;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new CutletCo();
+        }
+}
